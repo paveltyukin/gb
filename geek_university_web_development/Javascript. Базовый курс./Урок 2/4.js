f323f19d546bd7db5c5dfa139f317fa713cc9442
Pavel Tyukin <tyukin.pavel@yandex.ru>
2021-08-03 20:14:26 +0300

    js 2 lesson

---
+++ geek_university_web_development/Javascript. Базовый курс./Урок 2/4.js
@@ -0,0 +1,15 @@
+export const calculateSumOfNumbers = (number1, number2) => {
+    return number1 + number2
+};
+
+export const calculateSubtractionOfNumbers = (number1, number2) => {
+    return number1 - number2
+};
+
+export const calculateMultiplicationOfNumbers = (number1, number2) => {
+    return number1 * number2;
+};
+
+export const calculateDivisionOfNumbers = (number1, number2) => {
+    return number1 / number2;
+};
