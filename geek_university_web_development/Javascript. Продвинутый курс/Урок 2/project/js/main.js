f07357ad573a4e9af83f8c12d8251ce1698a3665
Pavel Tyukin <tyukin.pavel@yandex.ru>
2021-09-11 06:03:36 +0300

    js2_2 ready

--- geek_university_web_development/Javascript. Продвинутый курс/Урок 2/project/js/main.js
+++ geek_university_web_development/Javascript. Продвинутый курс/Урок 2/project/js/main.js
@@ -1,58 +1,16 @@
-class ProductList {
-  #goods;
-  #allProducts;
-
-  constructor(container = '.products') {
-    this.container = container;
-    this.#goods = [];
-    this.#allProducts = [];
-
-    this.#fetchGoods();
-    this.#render();
-  }
-
-  #fetchGoods() {
-    this.#goods = [
-      {id: 1, title: 'Notebook', price: 20000},
-      {id: 2, title: 'Mouse', price: 1500},
-      {id: 3, title: 'Keyboard', price: 5000},
-      {id: 4, title: 'Gamepad', price: 4500},
-    ];
-  }
-
-  #render() {
-    const block = document.querySelector(this.container);
-
-    for (let product of this.#goods) {
-      const productObject = new ProductItem(product);
-
-      this.#allProducts.push(productObject);
-      block.insertAdjacentHTML('beforeend', productObject.render());
-    }
-  }
-}
-
-class ProductItem {
-  constructor(product, img = 'https://via.placeholder.com/200x150') {
-    this.title = product.title;
-    this.price = product.price;
-    this.id = product.id;
-    this.img = img;
-  }
-
-  render() {
-    return `<div class="product-item" data-id="${this.id}">
-              <img src="${this.img}" alt="Some img">
-              <div class="desc">
-                  <h3>${this.title}</h3>
-                  <p>${this.price} \u20bd</p>
-                  <button class="buy-btn">Купить</button>
-              </div>
-          </div>`;
-  }
-}
+import { ProductList } from "./ProductList.js";
+import { Hamburger } from "./Hamburger.js";
 
 const list = new ProductList();
+console.log(list.getTotalCostOfProducts());
+
+const hamburger = new Hamburger();
+hamburger.addTopping("Картофель")
+console.log(hamburger.getSize());
+console.log(hamburger.getToppings());
+console.log(hamburger.getStuffing());
+console.log(hamburger.calculatePrice());
+console.log(hamburger.calculateCalories());
 
 // class ProductList {
 //   constructor(container = '.products') {
