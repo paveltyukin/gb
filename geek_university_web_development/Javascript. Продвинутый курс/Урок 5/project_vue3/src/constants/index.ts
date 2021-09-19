221b56f24e8f37be20e3077d6f5c206db4b85839
Pavel Tyukin <tyukin.pavel@yandex.ru>
2021-09-19 10:19:31 +0300

    js2_5 ready

--- geek_university_web_development/Javascript. Продвинутый курс/Урок 5/project_vue3/src/constants/index.ts
+++
@@ -1,5 +0,0 @@
-export const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
-
-export const CATALOG_URL = '/catalogData.json';
-
-export const PRODUCT_IMG = 'https://via.placeholder.com/200x150';
--- geek_university_web_development/Javascript. Продвинутый курс/Урок 5/project_vue3/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import { store } from './store';
-
-const app = createApp(App);
-// pass the injection key
-app.use(store)
-app.mount('#app')
